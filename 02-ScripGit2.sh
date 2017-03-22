d757cd091a0019c80b12c4af337408967e50037b
EPG System user <epg@demo-epg-mgrv-01.hi.inet>
2017-03-22 12:39:35 +0100

    Subete por favor

---
+++ 02-ScripGit2.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+#
+# Sube un fichero a git, cuyo nombre es pasado por parametro. tambien solicita el comentario para el 'commit'
+
+# Comprobamos que el parametro pasado ews un fichero
+if [ -f $1 ];
+then
+
+#Añadimos el archivo
+git add $1
+
+echo "Comentario: "
+read comentario
+
+
+# Creamos el commit de los cambios
+git commit -m "$comentario"
+
+
+#Guardamos en el repositorio remoto
+#git push origin master
+
+
+else
+echo "Este fichero no existe..."
+fi
+
+
